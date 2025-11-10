4558fa8d6c2ac51604dcdd5eadb02e8971de8382
nishita-esrat <nishitaesrat120@gmail.com>
2025-11-10 20:49:44 +0600

    triangle pattern

--- pattern2/pattern.cpp
+++ pattern2/pattern.cpp
@@ -5,8 +5,32 @@ using namespace std;
 
 int main()
 {
+    int n;
+    
+    cout << "triangle pattern " << endl;
 
-    cout << " pattern 2";
+    cout << "enter number : " << endl;
+
+    cin >> n;
+
+    for (int row = 1; row <= n; row++)
+    {
+        /* print space */
+
+        for (int col = 0; col <= n - row; col++)
+        {
+            cout << " ";
+        }
+
+        /* print (*) */
+
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
